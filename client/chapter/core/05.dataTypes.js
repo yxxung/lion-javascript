4243fa8e4015a6dcaa63064c4405237201d8b8aa
yxxung <loveus011@naver.com>
2023-07-05 10:35:36 +0900

    ECMAScript의 8가지 데이터 타입

--- client/chapter/core/05.dataTypes.js
+++ client/chapter/core/05.dataTypes.js
@@ -16,21 +16,18 @@
 
 // 6. undefined
 
-// 7. 객체와 심볼
+// 7. 객체(array, function..)와 심볼
 // 객체형을 제외한 다른 자료형은 문자열이든 숫자든 한 가지만 표현할 수 있기 때문에 원시 자료형이라 부릅니다
 // 반면 객체는 데이터 컬렉션이나 복잡한 개체를 표현할 수 있습니다
+
+// 8. 심볼
 // 심볼형은 객체의 고유한 식별자를 만들 때 사용됩니다
 
-// 8. typeof
+// 8+. typeof
 // 인수의 자료형을 반환
 // Math -> 수학 연산을 제공하는 내장 객체라 object 반환
 // null -> 하위 호환성을 위해 오류지만 남겨둠. 객체 아님
 // function -> function 반환
-
-
-
-
-
 
 /* ------------------------ */
 /* Data Types               */
@@ -39,31 +36,75 @@
 /* ECMAScript의 8가지 데이터 타입 -------------------------------------------- */
 
 // 1. 존재하지 않는(nothing) 값 / 비어있는(empty) 값 / 알 수 없는(unknown) 값
-// 2. 값이 할당되지 않은 상태
-// 3. 따옴표를 사용해 묶은 텍스트(큰", 작은', 역`)
-// 4. 정수, 부동 소수점 숫자(길이 제약)
-// 5. 길이에 제약이 없는 정수(예: 암호 관련 작업에서 사용)
-// 6. 참(true, yes) 또는 거짓(false, no)
-// 7. 데이터 컬렉션(collection) 또는 복잡한 엔티티(entity)
-// 8. 고유한 식별자(unique identifier)
+const empty = null;
 
+// 2. 값이 할당되지 않은 상태
+let unknown;
+
+// 3. 따옴표를 사용해 묶은 텍스트(큰", 작은', 역`)
+const hi = new String('hello'); // 문자 생성자(constructor)
+// 생성자로 string을 만들 시 데이터 타입은 object
+
+const double = 'hello'; // 문자 리터럴
+const single = 'hello';
+const backtick = `hello  ${1 + 3}`;
+
+// 4. 정수, 부동 소수점 숫자(길이 제약)
+const number = new Number(12323);
+
+const integer = 123;
+const floatingPointNumber = 10.45;
+
+// 5. 길이에 제약이 없는 정수(예: 암호 관련 작업에서 사용)
+const big = BigInt(123);
+
+const bigInteger = 123n;
+
+// 6. 참(true, yes) 또는 거짓(false, no)
+const bool = true;
+
+// 7. 데이터 컬렉션(collection) 또는 복잡한 엔티티(entity)
+const Obj = new Object({});
+
+const obj = {};
+
+// 8. 고유한 식별자(unique identifier)
+const unique = Symbol('uid');
+console.log(typeof unique);
 
 /* typeof 연산자의 2가지 사용법 ---------------------------------------------- */
 
 // 1) 연산자 typeof
 // 2) 함수 typeof()
 
+console.log(typeof (1 + 2));
+
 // 언어 상, 오류
 
-
-
-
 // Object
+const user = {
+  // 객체의 properties
+  name: 'tiger',
+  age: 28,
+  // 객체의 메소드
+  sayHi: function () {
+    console.log('say hi');
+  },
+};
 
 // Array
+const newArray = new Array();
+
+const arr = [10, 100, 1000, 1, 2, 3];
+
+const count = 0;
+
+`li:nth-child(${count + 1})`;
 
 // function
+function 붕어빵틀() {
+  
+}
+
 
 // this
-
-
